aa93c9de4f7f6834726ba109f24df8d641ff3857
ilias_bikenov <ilias.bikenov@gmail.com>
2022-03-05 12:43:39 +0600

    add user recognition

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,21 @@
 import { Layout } from 'antd';
-import { FC } from 'react';
+import { FC, useEffect } from 'react';
 import AppRouter from './components/AppRouter';
 import Navbar from './components/Navbar';
 import './App.css';
+import { useActions } from './hooks/useActions';
+import { IUser } from './models/IUser';
 
 const App: FC = () => {
+  const { setUser, setIsAuth } = useActions();
+  useEffect(() => {
+    //server auth imitating
+    if (localStorage.getItem('auth')) {
+      setUser({ username: localStorage.getItem('username' || '') } as IUser);
+      setIsAuth(true);
+    }
+  }, []);
+
   return (
     <Layout>
       <Navbar />
